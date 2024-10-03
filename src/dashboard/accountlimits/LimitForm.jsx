0328f1aa0c72969fb1c70f1fe01131c814a730c4
Ultimate Guru <tosindaniel360@gmail.com>
2024-10-03 14:49:48 +0100

    done with the form for assurance of changing the account limit

--- src/dashboard/accountlimits/LimitForm.jsx
+++ src/dashboard/accountlimits/LimitForm.jsx
@@ -1,12 +1,33 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 
-const LimitForm = () => {
+function RequestLimitForm() {
   return (
-    <div>
-        
+    <div className="bg-neutral-100 p-10 flex justify-center items-center">
+      {/* Form Card */}
+      <div className="bg-white p-8 rounded-xl shadow-lg max-w-md w-full h-96">
+        {/* Reason for Request */}
+        <div className="mb-6 mt-4">
+            <label className="block text-gray-700 text-sm font-semibold mb-2">Reason for request</label>
+            <select className="outline-none text-sm w-full p-3 bg-gray-100 text-gray-400 rounded-md">
+                <option>-Select-</option>
+                <option>Reason 1</option>
+                <option>Reason 2</option>
+                <option>Reason 3</option>
+            </select>
+        </div>
+
+        {/* Enter Limit */}
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-semibold mb-2">Enter limit</label>
+          <input type="number" className="outline-none w-full p-3 bg-gray-100 rounded-md"/>
+        </div>
+
+        {/* Proceed Button */}
+        <button className="bg-red-600 text-white font-bold p-3 w-52 ml-24 mt-9 rounded-md">Proceed</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LimitForm
+export default RequestLimitForm;
