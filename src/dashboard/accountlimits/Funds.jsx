44f1d11408f5dfde27c265d18be95d1a6d52f418
Ultimate Guru <tosindaniel360@gmail.com>
2024-10-03 12:35:29 +0100

    added components for the AccountLimits

---
+++ src/dashboard/accountlimits/Funds.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+function Funds() {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Funds;
---
+++ src/dashboard/accountlimits/Limits.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+function Limits() {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Limits;
