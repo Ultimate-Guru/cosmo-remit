62c19c521807dd16f83ee89f0a04dbdc242f9d4a
Ultimate Guru <tosindaniel360@gmail.com>
2024-10-04 13:26:19 +0100

    formatted some codes

--- src/dashboard/accountlimits/Review.jsx
+++ src/dashboard/accountlimits/Review.jsx
@@ -4,9 +4,9 @@ import ReviewImg from '../../assets/img/Frame 317.png';
 import HomeArrow from '../../assets/img/Frame 257.png';
 
 function Review() {
-    return(
+    return (
         <div className='h-screen flex flex-col items-center justify-center p-6 relative'>
-            
+
             {/* Go home link */}
             <div className="absolute top-5 left-5 flex items-center space-x-2 cursor-pointer">
                 <img src={HomeArrow} alt="Go back" />
@@ -14,15 +14,15 @@ function Review() {
 
             {/* Review Image */}
             <img src={ReviewImg} className="mb-8" alt="Limit under review" />
-            
+
             {/* Title */}
             <h2 className='text-2xl font-extrabold mb-4'>
                 Limit increase under review
             </h2>
-            
+
             {/* Description */}
             <p className='text-gray-500 text-center'>
-                Your request to upgrade account limits is currently <br /> 
+                Your request to upgrade account limits is currently <br />
                 being processed. Please check back later
             </p>
         </div>
