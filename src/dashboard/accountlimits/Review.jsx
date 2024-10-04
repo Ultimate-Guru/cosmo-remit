0a7fca3fbbc9d7fda4d493ccf9c71909347f9c45
Ultimate Guru <tosindaniel360@gmail.com>
2024-10-04 13:25:05 +0100

    review and success codes for the account limit

--- src/dashboard/accountlimits/Review.jsx
+++ src/dashboard/accountlimits/Review.jsx
@@ -1,10 +1,30 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import ReviewImg from '../../assets/img/Frame 317.png';
+import HomeArrow from '../../assets/img/Frame 257.png';
 
 function Review() {
     return(
-        <div>
+        <div className='h-screen flex flex-col items-center justify-center p-6 relative'>
+            
+            {/* Go home link */}
+            <div className="absolute top-5 left-5 flex items-center space-x-2 cursor-pointer">
+                <img src={HomeArrow} alt="Go back" />
+            </div>
 
+            {/* Review Image */}
+            <img src={ReviewImg} className="mb-8" alt="Limit under review" />
+            
+            {/* Title */}
+            <h2 className='text-2xl font-extrabold mb-4'>
+                Limit increase under review
+            </h2>
+            
+            {/* Description */}
+            <p className='text-gray-500 text-center'>
+                Your request to upgrade account limits is currently <br /> 
+                being processed. Please check back later
+            </p>
         </div>
     );
 }
--- src/dashboard/accountlimits/Success.jsx
+++ src/dashboard/accountlimits/Success.jsx
@@ -1,12 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import SuccessImg from "../../assets/img/succes-mark.png";
 
 function Success() {
-    return(
-        <div>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center p-5">
+      {/* Succes Image */}
+      <img src={SuccessImg} />
 
-        </div>
-    );
+      {/* Title */}
+      <h2 className="text-2xl font-extrabold mb-4">
+        Limit increased successfully
+      </h2>
+
+      {/* Description */}
+      <p className="text-gray-500 text-center font-semibold">
+        Your account limit has been increased
+      </p>
+
+      {/* Done button */}
+      <button className="bg-red-600 text-white font-bold rounded-md w-52 py-3 px-16 mt-12">
+        Done
+      </button>
+    </div>
+  );
 }
 
 export default Success;
