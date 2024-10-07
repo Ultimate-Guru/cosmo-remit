443ac18693a244d8ed6db4805cbdb48d2118e04c
Ultimate Guru <tosindaniel360@gmail.com>
2024-10-07 09:19:23 +0100

    restructured the routes and route in the app.jsx fro one componenet to show one at a time

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,75 @@
 /* eslint-disable no-unused-vars */
+// /* eslint-disable no-unused-vars */
+// import React from "react";
+// import Navbar from "../src/components/Navbar/Navbar";
+// import Hero from "./components/HeroSection/Hero";
+// import Choose from "./components/WhyChooseUs/Choose";
+// import Closer from "./components/CloserThanYouThink/Closer";
+// import Works from "./components/HowItWorks/Works";
+// import Countries from "./components/CountriesSection/Countries";
+// import Distance from "./components/DistanceSection/Distance";
+// import Fastest from "./components/FastestWaySection/Fastest";
+// import Faq from "./components/NewsFAQ/Faq";
+// import Apps from "./components/AppDownloadSection/Apps";
+// import Footer from "./components/Subscribe/Footer";
+// import SignUp from "./pages/SignUp";
+// import SignIn from "./pages/SignIn";
+// import { Routes, Route } from "react-router-dom";
+// import VerifyEmail from "./pages/VerifyEmail";
+// import Upload from "./pages/Upload";
+// import Passport from "./pages/Passport";
+// import Resident from "./pages/Resident";
+// import Transact from "./pages/Transact";
+// import Confirm from "./pages/Confirm";
+// import Success from "./dashboard/accountlimits/Success";
+// import Review from "./dashboard/accountlimits/Review";
+// import Funds from "./dashboard/accountlimits/Funds";
+// import Residence from "./dashboard/accountlimits/Residence";
+// import Limits from "./dashboard/accountlimits/Limits";
+// import LimitForm from "./dashboard/accountlimits/LimitForm";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Navbar />
+//       <Hero />
+//       <Choose />
+//       <Closer />
+//       <Works/>
+//       <Countries />
+//       <Distance />
+//       <Fastest />
+//       <Faq />
+//       <Apps />
+//       <Footer />
+
+//       <Routes>
+//         <Route path="/SignIn" element={<SignIn />} />
+//         <Route path="/SignUp" element={<SignUp />} />
+//       </Routes>
+
+//       <VerifyEmail />
+//       <Upload />
+//       <Passport />
+//       <Resident />
+//       <Transact />
+//       <Confirm />
+
+//       <Limits />
+//       <LimitForm />
+//       <Residence />
+//       <Funds />
+//       <Review />
+//       <Success />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "../src/components/Navbar/Navbar";
 import Hero from "./components/HeroSection/Hero";
 import Choose from "./components/WhyChooseUs/Choose";
@@ -13,7 +83,6 @@ import Apps from "./components/AppDownloadSection/Apps";
 import Footer from "./components/Subscribe/Footer";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
-import { Routes, Route } from "react-router-dom";
 import VerifyEmail from "./pages/VerifyEmail";
 import Upload from "./pages/Upload";
 import Passport from "./pages/Passport";
@@ -30,36 +99,45 @@ import LimitForm from "./dashboard/accountlimits/LimitForm";
 const App = () => {
   return (
     <div>
-      {/* <Navbar />
-      <Hero />
-      <Choose />
-      <Closer />
-      <Works/>
-      <Countries />
-      <Distance />
-      <Fastest />
-      <Faq />
-      <Apps />
-      <Footer />
-
       <Routes>
+        {/* Landing Page Route */}
+        <Route 
+          path="/" 
+          element={
+            <>
+              <Navbar />
+              <Hero />
+              <Choose />
+              <Closer />
+              <Works />
+              <Countries />
+              <Distance />
+              <Fastest />
+              <Faq />
+              <Apps />
+              <Footer />
+            </>
+          } 
+        />
+
+        {/* Authentication and other routes */}
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/VerifyEmail" element={<VerifyEmail />} />
+        <Route path="/Upload" element={<Upload />} />
+        <Route path="/Passport" element={<Passport />} />
+        <Route path="/Resident" element={<Resident />} />
+        <Route path="/Transact" element={<Transact />} />
+        <Route path="/Confirm" element={<Confirm />} />
+
+        {/* Dashboard-related Routes */}
+        <Route path="/Limits" element={<Limits />} />
+        <Route path="/LimitForm" element={<LimitForm />} />
+        <Route path="/Residence" element={<Residence />} />
+        <Route path="/Funds" element={<Funds />} />
+        <Route path="/Review" element={<Review />} />
+        <Route path="/Success" element={<Success />} />
       </Routes>
-
-      <VerifyEmail />
-      <Upload />
-      <Passport />
-      <Resident />
-      <Transact />
-      <Confirm /> */}
-
-      <Limits />
-      <LimitForm />
-      <Residence />
-      <Funds />
-      <Review />
-      <Success />
     </div>
   );
 };
