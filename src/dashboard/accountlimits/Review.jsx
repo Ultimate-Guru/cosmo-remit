37e8b33b9a8363ed076dc89e34265851a8912ddd
Ultimate Guru <tosindaniel360@gmail.com>
2024-10-08 11:04:33 +0100

    // Set a timeout to navigate to the success page after 10 seconds

--- src/dashboard/accountlimits/Review.jsx
+++ src/dashboard/accountlimits/Review.jsx
@@ -1,32 +1,44 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import ReviewImg from '../../assets/img/Frame 317.png';
-import HomeArrow from '../../assets/img/Frame 257.png';
+/* eslint-disable no-unused-vars */  
+import React, { useEffect } from 'react';  
+import ReviewImg from '../../assets/img/Frame 317.png';  
+import HomeArrow from '../../assets/img/Frame 257.png';  
+import { useNavigate } from 'react-router-dom';  
 
-function Review() {
-    return (
-        <div className='h-screen flex flex-col items-center justify-center p-6 relative'>
+function Review() {  
+    const navigate = useNavigate();  
 
-            {/* Go home link */}
-            <div className="absolute top-5 left-5 flex items-center space-x-2 cursor-pointer">
-                <img src={HomeArrow} alt="Go back" />
-            </div>
+    useEffect(() => {  
+        // Set a timeout to navigate to the success page after 10 seconds  
+        const timer = setTimeout(() => {  
+            navigate('/Success'); 
+        }, 10000); 
+        
+        // Cleanup the timer on component unmount  
+        return () => clearTimeout(timer);  
+    }, [navigate]);  
 
-            {/* Review Image */}
-            <img src={ReviewImg} className="mb-8" alt="Limit under review" />
+    return (  
+        <div className='h-screen flex flex-col items-center justify-center p-6 relative'>  
+            {/* Go home link */}  
+            <div className="absolute top-5 left-5 flex items-center space-x-2 cursor-pointer">  
+                <img src={HomeArrow} alt="Go back" />  
+            </div>  
 
-            {/* Title */}
-            <h2 className='text-2xl font-extrabold mb-4'>
-                Limit increase under review
-            </h2>
+            {/* Review Image */}  
+            <img src={ReviewImg} className="mb-8" alt="Limit under review" />  
 
-            {/* Description */}
-            <p className='text-gray-500 text-center'>
-                Your request to upgrade account limits is currently <br />
-                being processed. Please check back later
-            </p>
-        </div>
-    );
-}
+            {/* Title */}  
+            <h2 className='text-2xl font-extrabold mb-4'>  
+                Limit increase under review  
+            </h2>  
+
+            {/* Description */}  
+            <p className='text-gray-500 text-center'>  
+                Your request to upgrade account limits is currently <br />  
+                being processed. Please check back later  
+            </p>  
+        </div>  
+    );  
+}  
 
 export default Review;
