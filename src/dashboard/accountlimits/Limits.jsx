1271bd6207bba515709078fa293bd2ac4680febd
Ultimate Guru <tosindaniel360@gmail.com>
2024-10-08 11:05:35 +0100

    changed the stylings and  // Function to handle the clicking event fro the next page

--- src/dashboard/accountlimits/Limits.jsx
+++ src/dashboard/accountlimits/Limits.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Limits() {
+  const navigate = useNavigate();
+
+  // Function to handle the clicking event
+  const handleNextPage = () => {
+    navigate('/LimitForm')
+  }
+
   return (
-    <div className="bg-neutral-100 p-10">
+    <div className="bg-neutral-100 pt-32 pl-40 h-screen">
       {/* Account Limits Title */}
       <h1 className="font-extrabold text-xl text-left ml-40 mb-6">Account Limits</h1>
       
@@ -16,17 +24,17 @@ function Limits() {
           {/* Daily Limit */}
           <div className="text-left">
             <p className="text-gray-400 font-semibold">Daily limit</p>
-            <p className="text-black text-lg">20,000 UK</p>
+            <p className="text-black font-semibold text-lg">20,000 UK</p>
           </div>
 
           {/* Yearly Limit */}
           <div className="text-left">
             <p className="text-gray-400 font-semibold">Yearly limit</p>
-            <p className="text-black text-lg">1,000,000 UK</p>
+            <p className="text-black font-semibold text-lg">1,000,000 UK</p>
           </div>
 
           {/* Increase Limits Button */}
-          <button className="bg-red-600 text-white font-bold p-3 rounded-md w-48">
+          <button className="bg-red-600 text-white font-bold p-3 rounded-md w-48" onClick={handleNextPage}>
             Increase Limits
           </button>
         </div>
