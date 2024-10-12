5445e9adb26f0636fd5f5af2a743fd2e64256fa5
Ultimate Guru <tosindaniel360@gmail.com>
2024-10-12 19:24:14 +0100

    uncommented codes

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ import Funds from "./dashboard/accountlimits/Funds";
 import Residence from "./dashboard/accountlimits/Residence";
 import Limits from "./dashboard/accountlimits/Limits";
 import LimitForm from "./dashboard/accountlimits/LimitForm";
+import Transactions from "./dashboard/Transactions";
 
 const App = () => {
   return (
@@ -68,6 +69,7 @@ const App = () => {
         <Route path="/Funds" element={<Funds />} />
         <Route path="/Review" element={<Review />} />
         <Route path="/Success" element={<Success />} />
+        <Route path="/Transactions" element={<Transactions />} />
       </Routes>
     </div>
   );
