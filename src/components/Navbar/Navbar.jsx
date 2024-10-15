3407fecef8749928d74ff52e43eb2ea1fd339a02
Ultimate Guru <tosindaniel360@gmail.com>
2024-10-15 13:53:08 +0100

    gave it the right borders and fonts

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,23 +4,20 @@ import { NavLink } from 'react-router-dom'
 
 function Navbar() {
     const BgColor = {
-        backgroundColor: 'hsla(14, 56%, 49%, 0.8)'
+        backgroundColor: 'rgba(73, 48, 48, 0.84)'
     }
     return(
         <div style={BgColor} className="flex justify-between items-center p-3">
-            <img 
-            className="w-44" 
-            src="https://cosmoremit.com.au/assets/images/logo/logo.png" 
-            alt="Logo" />
+            <h2 className="text-white font-bold text-4xl ml-11">Logo</h2>
 
             <div className="flex">
                 <NavLink 
-                    className='mr-10 w-24 text-center bg-white text-red-600 p-2 rounded font-semibold' 
+                    className='mr-10 w-36 text-center bg-white text-red-600 p-2 rounded-lg font-semibold' 
                     to={'./SignIn'}>
                     Login
                 </NavLink>
                 <NavLink 
-                    className='text-white text-center w-28 mr-10 bg-red-600 font-semibold p-2 rounded' 
+                    className='text-white text-center w-36 mr-10 bg-red-600 font-semibold p-2 rounded-lg' 
                     to={'./SignUp'}>
                     Register
                 </NavLink>
