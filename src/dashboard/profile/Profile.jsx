6788f810ee47da9eee5fb368d51d266c6280d4fc
Ultimate Guru <tosindaniel360@gmail.com>
2024-11-06 13:46:16 +0100

    removed some crap

--- src/dashboard/profile/Profile.jsx
+++ src/dashboard/profile/Profile.jsx
@@ -1,14 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import PImg from '../../assets/img/brock-wegner.png'
-import { useLocation } from 'react-router-dom';
 
 const Profile = () => {
-  const location = useLocation();
-
-  // Access passed data
-  const { email, FirstName, LastName, Country } = location.state || {};
-
   return (
     <div>
       <div className=''>
@@ -17,7 +11,7 @@ const Profile = () => {
           <div className='flex justify-between items-center flex-row gap-3'>
             <div className='inline-flex items-center gap-9'>
               <img src={PImg} />
-              <h1 className='font-medium text-3xl'>{FirstName} {LastName}</h1>
+              <h1 className='font-medium text-3xl'>Kabir Akinola</h1>
             </div>
             <button
               className='w-40 h-12 leading-7 rounded-xl py-2 px-9 gap-3 bg-red-600 font-normal text-base text-white'
@@ -34,24 +28,24 @@ const Profile = () => {
             <div className='flex justify-between w-[477px] h-16 mt-6'>
               <div className='flex flex-col w-24 h-16 gap-3'>
                 <h3 className='font-normal text-lg text-gray-500'>First name</h3>
-                <span className='text-xl font-normal text-black'>{FirstName}</span>
+                <span className='text-xl font-normal text-black'>Kabir</span>
               </div>
 
               <div className='flex flex-col w-24 h-16 gap-3'>
                 <h3 className='font-normal text-lg text-gray-500'>Last name</h3>
-                <span className='text-xl font-normal text-black'>{LastName}</span>
+                <span className='text-xl font-normal text-black'>Akinola</span>
               </div>
             </div>
 
             <div className='flex justify-between w-[645px] h-16 mt-7'>
               <div className='flex flex-col w-48 h-16 gap-3'>
                 <h3 className='font-normal text-lg text-gray-500'>Country of residence</h3>
-                <span className='text-xl font-normal text-black'>{Country}</span>
+                <span className='text-xl font-normal text-black'>United Kingdom</span>
               </div>
 
               <div className='flex flex-col w-72 h-16 gap-3'>
                 <h3 className='font-normal text-lg text-gray-500'>Email address</h3>
-                <span className='text-xl font-normal text-black'>{email}</span>
+                <span className='text-xl font-normal text-black'>Kabirakinola@gmail.com</span>
               </div>
             </div>
 
